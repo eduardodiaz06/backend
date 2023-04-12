8bc6cd9cd1a6894cd3ead19ea1e6b259592bd8d8
eduardo diaz <eduardodiaz06@gmail.com>
2023-04-11 20:02:03 -0400

    primera entraga back

---
+++ README.md
@@ -0,0 +1,9 @@
+# Primera Entrega Back
+ 
+En este primera entrega se incluye la clase ProductManager.
+Con metodos:
+addProduct (agregar producto al arreglo principal)
+getProduct (devuelve el arreglo con los productos)
+getProductById (para buscar el producto por el id y si no se encuentra que tire un mensaje de Not Found)
+ 
+
---
+++ index.js
@@ -0,0 +1,61 @@
+class ProductManager {
+    constructor() {
+        this.products = []
+    }
+
+    addProduct({ title, description, price, thumbnail, stock }) {
+        let id = 0
+        if (this.products.length === 0) {
+            id = 1
+        } else {
+
+            let lastProducts = this.products[this.products.length - 1]
+            id = lastProducts.id + 1
+        }
+
+        let producto = { title, description, price, thumbnail, stock, id }
+        this.products.push(producto)
+    }
+
+    getProduct() {
+        console.log(this.products)
+        return this.products
+    }
+
+    getProductById(id) {
+        const product = this.products.find(product => product.id === id);
+        if (!product) {
+          console.error(`Not Found ${id}`);
+        }
+        return product;
+      }
+
+}
+
+let producto = new ProductManager()
+producto.addProduct({
+    title: "Gata Hidraulica",
+    description: "Gata para talleres automotrices",
+    price: 130000,
+    thumbnail: "https://aco.cl/imagenes/productos/047241.jpg", stock: 5
+})
+producto.addProduct({
+    title: "producto2",
+    description: "prueba",
+    price: 15000,
+    thumbnail: "https://aco.cl/imagenes/productos/047242.jpg", stock: 3
+})
+
+producto.addProduct({
+    title: "producto3",
+    description: "prueba",
+    price: 12000,
+    thumbnail: "https://aco.cl/imagenes/productos/047243.jpg", stock: 8
+})
+
+producto.getProduct()
+producto.getProductById(4)
+
+
+
+
