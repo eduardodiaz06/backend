d5c2e86dd8ca1ee39d6cb5ab6aa5411ce7c5e6ae
eduardo diaz <eduardodiaz06@gmail.com>
2023-05-16 21:25:51 -0400

    handlebars

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 import {Router} from "express"
-import carts_router from "./carts.router.js"
-import products_router from "./products.router.js"
+import api_router from '.api/index.js'
+//import  views_router from './views/index.js'
 
 const router = Router()
 
-router.use('/products',products_router)
-router.use('/carts',carts_router)
+router.use('/api',api_router)
+//router.use('/',views_router)
 
 export default router
